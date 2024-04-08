ae18db37625e7689fa8784447c9fbf2f3bae980c
sunjie <2942929854@qq.com>
2024-04-08 17:20:39 +0800

    修改app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
+<!--<template>-->
+<!--  <nav>-->
+<!--    <router-link to="/">首页</router-link> |-->
+<!--    <router-link to="/about">About</router-link>-->
+<!--  </nav>-->
+<!--  <router-view/>-->
+<!--</template>-->
+
+<!--<style>-->
+<!--#app {-->
+<!--  font-family: Avenir, Helvetica, Arial, sans-serif;-->
+<!--  -webkit-font-smoothing: antialiased;-->
+<!--  -moz-osx-font-smoothing: grayscale;-->
+<!--  text-align: center;-->
+<!--  color: #2c3e50;-->
+<!--}-->
+
+<!--nav {-->
+<!--  padding: 30px;-->
+<!--}-->
+
+<!--nav a {-->
+<!--  font-weight: bold;-->
+<!--  color: #2c3e50;-->
+<!--}-->
+
+<!--nav a.router-link-exact-active {-->
+<!--  color: #42b983;-->
+<!--}-->
+<!--</style>-->
+
+<!--学习用-->
+<!--组件的模板-->
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>app component</div>
 </template>
 
+<!--组件的行为-->、
+<script>
+
+</script>
+
+<!--组件的样式-->
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
